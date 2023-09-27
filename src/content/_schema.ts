4bc11b66bd36320b061b0834689115e20947c22d
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-09-27 11:20:32 +0800

    getCollection

---
+++ src/content/_schema.ts
@@ -0,0 +1,31 @@
+import { z, defineCollection } from "astro:content";
+
+const portfolioSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  image: z.string(),
+  link: z.string(),
+  tags: z.array(z.string()),
+});
+
+const authorSchema = z.object({
+  name: z.string(),
+  job: z.string(),
+  introduce: z.string(),
+  twitter: z.string(),
+  github: z.string(),
+  linkedin: z.string(),
+});
+const experienceSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  image: z.string(),
+  link: z.string(),
+  tags: z.array(z.string()),
+});
+
+export const collectionSchema = {
+  portfolioSchema,
+  authorSchema,
+  experienceSchema,
+};
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,25 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection } from "astro:content";
+import { collectionSchema } from "./_schema.ts";
+const { portfolioSchema, authorSchema, experienceSchema } = collectionSchema;
 // 2. Define your collection(s)
-const blogCollection = defineCollection({
+const portfolioCollection = defineCollection({
+  type: "content",
+  schema: portfolioSchema,
   /* ... */
+});
+const authorCollection = defineCollection({
+  type: "data",
+  schema: authorSchema,
+});
+const experienceCollection = defineCollection({
+  type: "content",
+  schema: experienceSchema,
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  blog: blogCollection,
+  "portfolio": portfolioCollection,
+  "authors": authorCollection,
+  "experience": experienceCollection,
 };

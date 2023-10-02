3f990b4bd3979b4c5b66087394b726a7f8b18997
steven31518 <107013204+steven31518@users.noreply.github.com>
2023-10-02 17:32:30 +0800

    created drawer

--- src/components/React/Drawer/index.tsx
+++ src/components/React/Drawer/index.tsx
@@ -1,59 +1,85 @@
-import Portal from "../Portal";
 import { motion, useCycle } from "framer-motion";
 import { useDimensions } from "../unit/useDimansions";
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import cn from "../unit/styleMerge";
 import Button from "../Button";
 import React, { useRef } from "react";
 const variants = {
-  open: (width = 100) => ({
-    opacity: 1,
-    width: width,
+  open: (height: number) => ({
+    width: 250,
+    height: height,
     x: 0,
     transition: {
       type: "spring",
-      stiffness: 400,
+      stiffness: 500,
       damping: 40,
+      delay: 0.2,
     },
   }),
-  closed: {
-    opacity: 0,
+  closed: (height: number) => ({
     width: 0,
-    height: 0,
-    x: "-100%",
+    height: height,
+    x: -100,
     transition: {
       type: "spring",
-      stiffness: 400,
+      stiffness: 500,
       damping: 40,
+      delay: 0.2,
+    },
+  }),
+};
+const iconVariants = {
+  open: {
+    rotateY: 180,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+      damping: 40,
+      delay: 0.2,
+    },
+  },
+  closed: {
+    rotateY: 0,
+    transition: {
+      type: "spring",
+      stiffness: 500,
+      damping: 40,
+      delay: 0.2,
     },
   },
 };
-
-type Props = {
-  children: React.ReactNode;
-};
-
 const Drawer = ({ children }: Props) => {
-  const [isOpen, toggleOpen] = useCycle(false, true);
+  const [isOpen, toggleOpen] = useCycle(true, false);
   const drawerRef = useRef(null);
-  const { width } = useDimensions(drawerRef);
+  const { height } = useDimensions(drawerRef);
+
   return (
     <>
       <motion.div
         initial={false}
         animate={isOpen ? "open" : "closed"}
-        custom={width}
-        ref={drawerRef}
+        custom={height}
+        className="fixed top-100 left-0 z-50"
       >
-        <div className="flex">
-          <motion.ul variants={variants} className="bg-white">
+        <div className="flex items-start rounded-e-sm">
+          <motion.ul
+            variants={variants}
+            ref={drawerRef}
+            className="text-center bg-slate-50"
+          >
             {children}
           </motion.ul>
-          <Button
-            variant="text"
-            themeColor="primary"
+          <motion.div
+            variants={iconVariants}
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 1 }}
+            className={cn(
+              "text-center cursor-pointer p-1 flex  bg-slate-50 border-2 border-solid border-slate-300",
+            )}
             onClick={() => toggleOpen()}
           >
-            {">>"}
-          </Button>
+            <AiOutlineMenuUnfold className="text-4xl" />
+          </motion.div>
         </div>
       </motion.div>
     </>
